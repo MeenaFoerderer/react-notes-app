831960fd99f1a43454b3f74914d8bce00ca9496f
MeenaFoerderer <meena.foerderer@gmail.com>
2023-03-21 09:55:44 +0100

    implement dynamic rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import NotesList from "./components/NotesList";
+import { useState } from "react";
+import {nanoid} from 'nanoid';
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is the first note",
+      date: "21/03/2023"
+    },
+    {
+      id: nanoid(),
+      text: "This is the second note",
+      date: "22/03/2023"
+    },
+    {
+      id: nanoid(),
+      text: "This is the third note",
+      date: "23/03/2023"
+    },
+    {
+      id: nanoid(),
+      text: "This is the fourth note",
+      date: "24/03/2023"
+    },
+  ]);
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes}/>
     </div>
   );
 }
