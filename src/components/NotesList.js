5d1bf7db0c18a05a66cc0aff0bbea0e0cad5a7cc
MeenaFoerderer <meena.foerderer@gmail.com>
2023-03-23 15:19:49 +0100

    add delete note functionality

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,12 +1,17 @@
 import Note from "./Note";
 import AddNote from "./AddNote";
 
-function NotesList({notes, handleAddNote}) {
+function NotesList({notes, handleAddNote, handleDeleteNote}) {
     return (
         <div className='notes-list'>
             <AddNote handleAddNote={handleAddNote}/>
             {notes.slice(0).reverse().map((note) => (
-                <Note id={note.id} text={note.text} date={note.date}/>
+                <Note 
+                    id={note.id} 
+                    text={note.text} 
+                    date={note.date}
+                    handleDeleteNote={handleDeleteNote}
+                />
             ))}
         </div>
     );
